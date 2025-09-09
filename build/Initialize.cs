764507d821f11059129acdd1e1eaf5903319ff8f
VNC <main@vnncc.dev>
2025-09-09 08:55:16 +0200

    feat: add module initializer to opt-out of NUKE telemetry

---
+++ build/Initialize.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace _build;
+
+public static class Initialize
+{
+    [ModuleInitializer]
+    public static void ModuleInitialize() => Environment.SetEnvironmentVariable("NUKE_TELEMETRY_OPTOUT", "1");
+}
